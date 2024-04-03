4284e411caca2ccb4eb86de0f9d0923ed00132b2
Jordan Yamada <sadaotomokane@gmail.com>
2024-04-03 02:49:46 +0000

    created apps for my models

--- ai_dventure_proj/settings.py
+++ ai_dventure_proj/settings.py
@@ -131,4 +131,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'user_app.User'
+# AUTH_USER_MODEL = 'user_app.User'
---
+++ progress_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProgressAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'progress_app'
