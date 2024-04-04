30cd0a0ab853f4c433742c181e6820836724dc50
Jordan Yamada <sadaotomokane@gmail.com>
2024-04-04 16:50:30 +0000

    starting on progress model

--- progress_app/models.py
+++ progress_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core import validators as v
+from story_app.models import Story
+# from item_app.models import Item
+
+
 
 # Create your models here.
+class Progress(models.Model):
+    
+    image = models.CharField(
+        blank=True,
+        null=True,
+    )
+    dialogue = models.CharField(
+        blank=True,
+        null=True,
+    )
+    choice_one = models.CharField(
+        blank=True,
+        null=True,
+    )
+    choice_two = models.CharField(
+        blank=True,
+        null=True,
+    )
+    choice_three = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    story = models.ForeignKey(Story, on_delete=models.SET_NULL, null=True, related_name="progress")
