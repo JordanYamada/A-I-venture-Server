4ef4f5d92c7aac842f91b5803c6670b1f99e2766
Jordan Yamada <sadaotomokane@gmail.com>
2024-04-04 16:39:54 +0000

    working on story model

--- story_app/models.py
+++ story_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core import validators as v
+from user_app.models import Client
+# from item_app.models import Item
+
+
 
 # Create your models here.
+class Story(models.Model):
+    
+    theme = models.CharField(
+        blank=True,
+        null=True,
+    )
+    role = models.CharField(
+        blank=True,
+        null=True,
+    )
+    title = models.CharField(
+        blank=True,
+        null=True,
+    )
+    role = models.BooleanField(
+        blank=True,
+        null=True,
+    )
+
+    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, related_name="stories")
