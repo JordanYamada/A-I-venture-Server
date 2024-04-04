511e996830cef8f3f2ea5bbfad6ed38a9900b62e
Jordan Yamada <sadaotomokane@gmail.com>
2024-04-04 16:56:46 +0000

    working on memory model

--- memory_app/models.py
+++ memory_app/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+from django.core import validators as v
+
+
+
+
+# Create your models here.
+class Progress(models.Model):
+    
+    image = models.CharField(
+        blank=True,
+        null=True,
+    )
+    dialogue = models.CharField(
+        blank=True,
+        null=True,
+    )
+    # choice_one = models.CharField(
+    #     blank=True,
+    #     null=True,
+    # )
+    # choice_two = models.CharField(
+    #     blank=True,
+    #     null=True,
+    # )
+    # choice_three = models.CharField(
+    #     blank=True,
+    #     null=True,
+    # )
