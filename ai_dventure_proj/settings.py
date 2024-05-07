9f35125b9411f2017411e2c9642ae23e2837b8e4
Jordan Yamada <sadaotomokane@gmail.com>
2024-05-07 01:42:04 +0000

    ajusted allowed hosts for render deploy

--- ai_dventure_proj/settings.py
+++ ai_dventure_proj/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 
 
-ALLOWED_HOSTS = [
-  os.environ.get("ALLOWED_HOSTS"),
-]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
 
 # Application definition
@@ -146,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
