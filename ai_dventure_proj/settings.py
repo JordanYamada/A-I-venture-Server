17870660bedd4813ae9678293eed5bf52e0bbd29
Jordan Yamada <sadaotomokane@gmail.com>
2024-05-07 15:46:16 +0000

    adjusted DATABASES in settings for deployment

--- ai_dventure_proj/settings.py
+++ ai_dventure_proj/settings.py
@@ -98,12 +98,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
-        # 'USER': os.environ.get('POSTGRES_USER'), 
-        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        # 'HOST': os.environ.get('POSTGRES_HOST', 'localhost'), 
-        # 'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'USER': os.environ.get('POSTGRES_USER'), 
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'), 
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
+
+# DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
